3e9e8f7156f822455bc0b27264467fc0dac88291
Abdullah Alabbad <abdullah@alabbad.dev>
2023-09-17 22:24:02 +1000

    bulk import posts from csv file

--- src/dev/alabbad/controllers/DashboardController.java
+++ src/dev/alabbad/controllers/DashboardController.java
@@ -1,13 +1,18 @@
 package dev.alabbad.controllers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.Scanner;
 
+import dev.alabbad.exceptions.PostNotFoundException;
 import dev.alabbad.exceptions.UserNotFoundException;
 import dev.alabbad.models.AppState;
 import dev.alabbad.models.DB;
 import dev.alabbad.models.VIPUser;
+import dev.alabbad.utils.Parser;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
@@ -15,19 +20,18 @@ import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
-import javafx.scene.control.Label;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 public class DashboardController extends VBox {
     public DashboardController() {
         // construct view based on the logged user
         if (AppState.getInstance().getUser() instanceof VIPUser) {
-            setupVIPUserView();
             try {
-                drawPieChart();
-                // TODO: Add import bulk posts button with implement its controller
+                setupVIPUserView();
             } catch (SQLException e) {
                 // TODO: handle error when
                 System.out.println("Couldn't get data of share distribution!");
@@ -44,33 +48,24 @@ public class DashboardController extends VBox {
         vipButton.getStylesheets().add("/css/button.css");
         vipButton.getStyleClass().add("button");
         vipButton.getStyleClass().add("primary");
-        vipButton.onMouseClickedProperty().set(event -> this.onPrimaryBtnClicked(event));
+        vipButton.onMouseClickedProperty().set(event -> this.onVIPBtnClicked(event));
         this.getChildren().add(vipButton);
     }
 
-    private void setupVIPUserView() {
-        this.getChildren().add(new Label("You're a VIP user"));
+    private void setupVIPUserView() throws SQLException {
+        // this.getChildren().add(new Label("You're a VIP user"));
+        drawPieChart();
+        Button importPostsButton = new Button("Import Posts");
+        importPostsButton.setId("import-posts-button");
+        importPostsButton.setMaxWidth(Double.MAX_VALUE);
+        importPostsButton.getStylesheets().add("/css/button.css");
+        importPostsButton.getStyleClass().add("button");
+        importPostsButton.getStyleClass().add("primary");
+        importPostsButton.onMouseClickedProperty().set(event -> this.onImportBtnClicked(event));
+        this.getChildren().add(importPostsButton);
     }
 
-    private void drawPieChart() throws SQLException {
-        // Preparing ObservbleList object
-        ObservableList<PieChart.Data> data = FXCollections.observableArrayList();
-        ArrayList<Integer> shareDisribution = DB.getSharesDistribution();
-
-        data.add(new PieChart.Data("0 - 99 Shares", shareDisribution.get(0)));
-        data.add(new PieChart.Data("100 - 999 Shares", shareDisribution.get(1)));
-        data.add(new PieChart.Data("> 1000 Shares", shareDisribution.get(2)));
-
-        // Creating a Pie chart
-        PieChart pieChart = new PieChart(data);
-        pieChart.setData(data);
-        pieChart.setTitle("Mobile Sales");
-        pieChart.setLabelLineLength(32);
-        pieChart.setStartAngle(90);
-        this.getChildren().add(pieChart);
-    }
-
-    public void onPrimaryBtnClicked(MouseEvent event) {
+    public void onVIPBtnClicked(MouseEvent event) {
         // display the dialgo for vip upgrade
         Dialog<ButtonType> dialog = new Dialog<ButtonType>();
         dialog.setTitle("VIP Upgrade");
@@ -95,4 +90,101 @@ public class DashboardController extends VBox {
             }
         }
     }
+
+    public void onImportBtnClicked(MouseEvent event) {
+        try {
+            File fileLocation = chooseFileLocation();
+            if (fileLocation != null) {
+                this.importPost(fileLocation);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found exception!");
+        }
+    }
+
+    private void drawPieChart() throws SQLException {
+        // Preparing ObservbleList object
+        ObservableList<PieChart.Data> data = FXCollections.observableArrayList();
+        ArrayList<Integer> shareDisribution = DB.getSharesDistribution();
+
+        data.add(new PieChart.Data("0 - 99 Shares", shareDisribution.get(0)));
+        data.add(new PieChart.Data("100 - 999 Shares", shareDisribution.get(1)));
+        data.add(new PieChart.Data("> 1000 Shares", shareDisribution.get(2)));
+
+        // Creating a Pie chart
+        PieChart pieChart = new PieChart(data);
+        pieChart.setData(data);
+        pieChart.setTitle("Share Distribution");
+        pieChart.setLabelLineLength(32);
+        pieChart.setStartAngle(90);
+        this.getChildren().add(pieChart);
+    }
+
+    private File chooseFileLocation() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(new ExtensionFilter("CSV Files ", "*.csv"));
+        File file = fileChooser.showOpenDialog(AppState.getInstance().getStage());
+        return file;
+    }
+
+    private void importPost(File file) throws FileNotFoundException {
+        // create scanner for the file
+        int expectedFieldsNum = 6;
+        int readRowsCount = 0;
+        int validRowsCount = 0;
+        Scanner scan = new Scanner(file);
+        // skip first row (headers)
+        if (scan.hasNextLine()) {
+            scan.nextLine();
+        }
+        // process each row
+        while (scan.hasNextLine()) {
+            try {
+                readRowsCount++;
+                String[] fields = parseCSV(scan.nextLine(), expectedFieldsNum);
+                int ID = Parser.parseInt(fields[0], 0);
+                try {
+                    // Skip if the there is an already existing post
+                    DB.getPost(ID);
+                    System.out.printf("Post with ID = %d already exist!\n", ID);
+                } catch (PostNotFoundException e) {
+                    // parse fields
+                    String content = fields[1];
+                    String author = Parser.parseStr(fields[2]);
+                    int likes = Parser.parseInt(fields[3], 0);
+                    int shares = Parser.parseInt(fields[4], 0);
+                    String dateTime = Parser.parseDateTime(fields[5]);
+                    // create post obj
+                    DB.insertPost(ID, content, author, likes, shares, dateTime);
+                    validRowsCount++;
+                }
+            } catch (Exception e) {
+                System.out.println("Expected " + expectedFieldsNum + " fields!");
+            }
+        }
+        System.out.printf("%d valid posts has been loaded\n", validRowsCount);
+        System.out.printf("%d invalid posts has been ignored\n", readRowsCount - validRowsCount);
+    }
+
+    /**
+     * Parse CSV string
+     *
+     * @param str the CSV string to be parsed
+     * @param expectedFieldsNum the number of expected fields
+     * @return array of strings
+     * @throws Exception
+     */
+    public static String[] parseCSV(String str, int expectedFieldsNum) throws Exception {
+        String[] fields = str.split(",");
+
+        if (fields.length != expectedFieldsNum) {
+            throw new Exception("Invalid number of fields");
+        }
+
+        for (int i = 0; i < fields.length; i++) {
+            fields[i] = fields[i].strip();
+        }
+        return fields;
+    }
+
 }
