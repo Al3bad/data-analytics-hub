99fbc3cb29b609821cc3b132a3e4b77d9d0966d7
Abdullah Alabbad <abdullah@alabbad.dev>
2023-10-03 22:17:56 +1100

    test DB.getUser() method

--- test/TestDB.java
+++ test/TestDB.java
@@ -25,6 +25,8 @@ public class TestDB {
         System.out.println("\n--> Test " + ++TestDB.testCount);
         DB.connect(":memory:");
         this.conn = DB.getConnection();
+        DB.createUserTable();
+        DB.createPostTable();
     }
 
     // ==================================================
@@ -49,25 +51,44 @@ public class TestDB {
     // ==================================================
     // --> Users: Insert user
     // ==================================================
+    @Test
     public void testInsertUser_User() throws SQLException, UserNotFoundException {
         User user = DB.insertUser("username", "123456789", "First", "Last", false);
         assertTrue(user instanceof User);
         assertEquals("username", user.getUsername());
     }
 
+    @Test
     public void testInsertUser_AdminUser() throws SQLException, UserNotFoundException {
         User adminUser = DB.insertUser("admin", "admin", "First", "Last", true);
         assertTrue(adminUser instanceof AdminUser);
         assertEquals("admin", adminUser.getUsername());
     }
 
+    @Test(expected = SQLException.class)
     public void testInsertUser_User_Exception() throws SQLException, UserNotFoundException {
-        User user = DB.insertUser(null, "123456789", "First", "Last", false);
-        assertTrue(user instanceof User);
-        assertEquals("username", user.getUsername());
+        DB.insertUser(null, "123456789", "First", "Last", false);
+    }
+
+    @Test(expected = SQLException.class)
+    public void getUserTest_Excpetion() throws SQLException, UserNotFoundException {
+        DB.insertUser("username", "123456789", "First", "Last", false);
+        DB.insertUser("username", "123456789", "First", "Last", false);
     }
 
     // ==================================================
     // --> Users: Get user
     // ==================================================
+    @Test
+    public void getUserTest() throws SQLException, UserNotFoundException {
+        DB.insertUser("username", "123456789", "First", "Last", false);
+        User user2 = DB.getUser("username");
+        assertTrue(user2 instanceof User);
+        assertEquals("username", user2.getUsername());
+    }
+
+    @Test(expected = UserNotFoundException.class)
+    public void getUserTest_UserNotFoundException() throws SQLException, UserNotFoundException {
+        DB.getUser("username");
+    }
 }
