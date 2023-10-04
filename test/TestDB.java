1cf8188d5db391e339d159a28354ff47e0f8b2f6
Abdullah Alabbad <abdullah@alabbad.dev>
2023-10-04 20:02:03 +1100

    test DB.getAllUsers() method

--- test/TestDB.java
+++ test/TestDB.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.*;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 
+import dev.alabbad.exceptions.UnauthorisedAction;
 import dev.alabbad.exceptions.UserNotFoundException;
 import dev.alabbad.models.AdminUser;
 import dev.alabbad.models.DB;
@@ -33,17 +35,17 @@ public class TestDB {
     // --> Create tables
     // ==================================================
     @Test
-    public void testCreateUserTable() {
+    public void createUserTableTest() {
         assertTrue(DB.createUserTable());
     }
 
     @Test
-    public void testCreatePostTable() {
+    public void createPostTableTest() {
         assertTrue(DB.createPostTable());
     }
 
     @Test
-    public void testCreatePostTable_Faild() throws SQLException {
+    public void createPostTableTest_Faild() throws SQLException {
         DB.getConnection().close();
         assertFalse(DB.createPostTable());
     }
@@ -52,21 +54,21 @@ public class TestDB {
     // --> Users: Insert user
     // ==================================================
     @Test
-    public void testInsertUser_User() throws SQLException, UserNotFoundException {
+    public void insertUserTest_User() throws SQLException, UserNotFoundException {
         User user = DB.insertUser("username", "123456789", "First", "Last", false);
         assertTrue(user instanceof User);
         assertEquals("username", user.getUsername());
     }
 
     @Test
-    public void testInsertUser_AdminUser() throws SQLException, UserNotFoundException {
+    public void insertUserTest_AdminUser() throws SQLException, UserNotFoundException {
         User adminUser = DB.insertUser("admin", "admin", "First", "Last", true);
         assertTrue(adminUser instanceof AdminUser);
         assertEquals("admin", adminUser.getUsername());
     }
 
     @Test(expected = SQLException.class)
-    public void testInsertUser_User_Exception() throws SQLException, UserNotFoundException {
+    public void insertUserTest_User_Exception() throws SQLException, UserNotFoundException {
         DB.insertUser(null, "123456789", "First", "Last", false);
     }
 
@@ -90,5 +92,31 @@ public class TestDB {
     @Test(expected = UserNotFoundException.class)
     public void getUserTest_UserNotFoundException() throws SQLException, UserNotFoundException {
         DB.getUser("username");
+    }
+
+    // ==================================================
+    // --> Users: Get all users
+    // ==================================================
+    @Test
+    public void getAllUsersTest() throws SQLException, UserNotFoundException {
+        HashMap<String, User> users;
+        users = DB.getAllUsers();
+        assertEquals(0, users.size());
+        DB.insertUser("username1", "123456789", "First", "Last", false);
+        DB.insertUser("username2", "123456789", "First", "Last", false);
+        DB.insertUser("username3", "123456789", "First", "Last", false);
+        DB.insertUser("username4", "123456789", "First", "Last", false);
+        DB.insertUser("username5", "123456789", "First", "Last", false);
+        users = DB.getAllUsers();
+        assertEquals(5, users.size());
+    }
+
+    // ==================================================
+    // --> Users: Update user
+    // ==================================================
+    @Test
+    public void updateUserTest() throws SQLException, UserNotFoundException, UnauthorisedAction {
+        User updatedUser = DB.updateUser("username", "newusername", "123456789", "123456789", "First", "Last");
+        assertEquals("newusername", updatedUser.getUsername());
     }
 }
